9563aff3ba99c7ed433983e6d1677ce5865f7dda
Rep Graphics <charliebeckingham@hotmail.com>
2025-04-16 15:21:06 +0100

    Discord Status'

--- src/components/Donators.jsx
+++ src/components/Donators.jsx
@@ -24,6 +24,21 @@ export default function Donators() {
       });
   }, []);
 
+  const getStatusColor = (status) => {
+    switch (status) {
+      case "online":
+        return "ring-success";
+      case "idle":
+        return "ring-warning";
+      case "dnd":
+        return "ring-error";
+      case "offline":
+        return "ring-neutral-content";
+      default:
+        return "ring-neutral-content";
+    }
+  };
+
   if (loading) {
     return (
       <section id="donators" className="py-16 bg-gray-900 text-white text-center">
@@ -52,11 +67,16 @@ export default function Donators() {
         className="glass text-white p-6 rounded-lg shadow-lg flex items-center justify-between w-120 min-w-120 max-w-120"
       >
         {/* User's Image on the Left */}
+        <div
+        className={`${getStatusColor(donator.status)} ring-offset-base-100 w-24 h-24 rounded-full ring ring-offset-2 tooltip tooltip-left`}
+        data-tip={donator.status}
+      >
         <img
           src={`https://api.euphoriatheme.uk${donator.avatar}`}
           alt={donator.name}
           className="w-24 h-24 rounded-full"
         />
+        </div>
 
         {/* Name and Tier in the Center */}
         <div className="flex flex-col items-center flex-1 px-4">
